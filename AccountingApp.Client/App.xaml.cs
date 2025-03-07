06fbabfbc5001504710f5112f385729ff40e22f0
Steve <steve.meerschaut@telenet.be>
2025-03-07 20:10:15 +0100

    Initial project structure with .gitignore

---
+++ AccountingApp.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountingApp.Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AccountingApp.Client/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountingApp.Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
